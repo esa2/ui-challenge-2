3e16849a75e1234c68f45c3e88763bfa349c4ed6
Ed Abrahamsen <esabrahamsen@yahoo.com>
2017-12-17 14:27:53 -0800

    add focus and blur function

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,18 @@
 'use strict';
 
+$('.in-border').on('focus', function() {
+  $('.red').addClass("in-border-focus"); // remove error text
+})
+
+$('.in-border').on('blur', function() {
+  let uname = $('.in-border').val();
+if(uname.length > 5) {
+  $('.in-border').removeClass("in-border-red"); // remove red border
+  } else {
+    $('.red').removeClass("in-border-focus"); // add error text
+  }
+})
 
 $('#input-radio').on('click', function() {
-    $('.radio1').addClass("input-radio-check");
+    $('.radio1').addClass("input-radio-check"); // add radio checked
   })
